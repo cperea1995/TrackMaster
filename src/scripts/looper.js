d6ff6db402e124e1ff5e3df6ef115bde650aca5d
cperea1995 <cperea1995@gmail.com>
2020-04-30 17:42:52 -0400

    fix null error

--- src/scripts/looper.js
+++ src/scripts/looper.js
@@ -1,36 +1,36 @@
-import * as Tone from 'tone';
-import "regenerator-runtime/runtime.js";
+// import * as Tone from 'tone';
+// import "regenerator-runtime/runtime.js";
 
-const synths = [
-    new Tone.Synth(),
-    new Tone.Synth(),
-    new Tone.Synth()
-];
+// const synths = [
+//     new Tone.Synth(),
+//     new Tone.Synth(),
+//     new Tone.Synth()
+// ];
 
-synths[0].oscillator.type = 'triangle';
-synths[0].oscillator.type = 'sine';
-synths[0].oscillator.type = 'sawtooth';
+// synths[0].oscillator.type = 'triangle';
+// synths[0].oscillator.type = 'sine';
+// synths[0].oscillator.type = 'sawtooth';
 
-synths.forEach(synth => synth.toMaster());
+// synths.forEach(synth => synth.toMaster());
 
-const rows = Array.from(document.querySelectorAll('.grid > div'));
-const notes = ['C4', 'D4', 'E4', 'F4', 'G4', 'A4', 'B4', 'C5']
-let index = 0
+// const rows = Array.from(document.querySelectorAll('.grid > div'));
+// const notes = ['C4', 'D4', 'E4', 'F4', 'G4', 'A4', 'B4', 'C5']
+// let index = 0
 
-Tone.Transport.scheduleRepeat(repeat, '8n');
-Tone.Transport.start();
+// Tone.Transport.scheduleRepeat(repeat, '8n');
+// Tone.Transport.start();
 
-function repeat(time) {
-    let step = index % 8
-    for (let i = 0; i < rows.length; i++) {
-        let synth = synths[i],
-        note = notes[i],
-        row = rows[i],
-        input = row.querySelector(`input:nth-child(${step + 1})`);
+// function repeat(time) {
+//     let step = index % 8
+//     for (let i = 0; i < rows.length; i++) {
+//         let synth = synths[i],
+//         note = notes[i],
+//         row = rows[i],
+//         input = row.querySelector(`input:nth-child(${step + 1})`);
 
-        if (input.checked) {
-            synth.triggerAttackRelease(note, '8n', time);
-        }
-    }
-    index++;
-}
+//         if (input.checked) {
+//             synth.triggerAttackRelease(note, '8n', time);
+//         }
+//     }
+//     index++;
+// }

3b9f4bea254ed80e9692c10732a8c97f1c60d676
cperea1995 <cperea1995@gmail.com>
2020-05-01 13:38:05 -0400

    add play/pause/reset feature

--- src/scripts/looper.js
+++ src/scripts/looper.js
@@ -37,9 +37,58 @@ window.addEventListener('DOMContentLoaded', () => {
     const notes = ['C4', 'D4', 'E4', 'F4', 'G4', 'A4', 'B4', 'C5']
     let index = 0;
 
+    const playButton = document.body.querySelector('.start-button');
+    const pauseButton = document.body.querySelector('.pause-button');
+    const clearButton = document.body.querySelector('.clear-button');
+
+
+    playButton.addEventListener('click', () => {
+        if (pauseButton.classList.contains('enabled') || clearButton.classList.contains('enabled')) {
+            pauseButton.classList.remove('enabled');
+            clearButton.classList.remove('enabled');
+        }
+        Tone.Transport.start();
+        playButton.classList.add('enabled')
+    })
+
+    pauseButton.addEventListener('click', () => {
+        if (playButton.classList.contains('enabled') || clearButton.classList.contains('enabled')) {
+            playButton.classList.remove('enabled');
+            clearButton.classList.remove('enabled');
+        }
+        Tone.Transport.pause();
+        pauseButton.classList.add('enabled')
+    })
+
+    clearButton.addEventListener('click', () => {
+        if (playButton.classList.contains('enabled') || pauseButton.classList.contains('enabled')) {
+            playButton.classList.remove('enabled');
+            pauseButton.classList.remove('enabled');
+        }
+        const inputs = Array.from(document.getElementsByTagName('input'));
+
+        for (let i = 0; i < inputs.length; i++) {
+            const input = inputs[i];
+
+            if (input.checked) {
+                input.checked = false;
+            }
+        }
+        Tone.Transport.stop();
+        clearButton.classList.add('enabled-clear');
+        document.body.querySelector('.clear-button').innerHTML = '<i class="fas fa-times"></i>'
+        setTimeout(removeEnableClass, 1000);
+    })
+
+    function removeEnableClass () {
+        clearButton.classList.remove('enabled-clear')
+        document.body.querySelector('.clear-button').innerHTML = '<i class="fas fa-eraser">'
+    }
+
+    
     // Tone.Transport.bpm.value = 240;
     Tone.Transport.scheduleRepeat(repeat, '8n')
-    Tone.Transport.start();
+    // Tone.Transport.start();
     
     function repeat(time) {
         let step = index % 8
